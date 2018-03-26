3a17547fca6fdbf3c1507ac9160d987456d16d3e
Yanuar <yanuar@softwareseni.com>
2018-03-26 18:32:02 +0700

    refactor mongoose, add POST /todos

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true
+    }
+});
+
+module.exports = User;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+let mongoose = require('./db/mongoose');
+
+let Todo = require('./models/todo');
+let User = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Todo App!');
+});
+
+app.post('/todos', (req, res) => {
+
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then(doc => res.send(doc), e => res.status(400).send(e));
+});
+
+app.post('/user', (req, res) => {
+
+    let user = new User({
+        email: req.body.email
+    });
+
+    user.save().then(doc => res.send(doc), e => res.status(400).send(e));
+});
+
+// start server
+app.listen(3000, () => {
+    console.log('Server up and running.');
+});
