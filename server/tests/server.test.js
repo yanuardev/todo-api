241f0ec67adef4f1ac931e522937058bd3875bd5
Yanuar <yanuar@softwareseni.com>
2018-03-27 00:12:49 +0700

    add test GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 let app = require('./../server');
 let Todo = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'learn 1'
 }, {
+    _id: new ObjectID(),
     text: 'learn 2'
 }];
 
@@ -75,4 +78,34 @@ describe('GET /todos', () => {
             })
             .end(done);
     });
-})
+});
+
+describe('GET /todos/:id', () => {
+
+    it('Should return todo', done => {
+
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('Should return 404 if not found', done => {
+
+        request(app)
+            .get(`/todos/${new ObjectID()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('Should return 404 if id not valid', done => {
+
+        request(app)
+            .get(`/todos/123`)
+            .expect(404)
+            .end(done);
+    });
+});
