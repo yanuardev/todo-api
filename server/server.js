257ea1e631ebd5e07987c20492eecfdd1c2c28d8
Yanuar <yanuar@softwareseni.com>
2018-03-26 23:56:41 +0700

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let mongoose = require('./db/mongoose');
 
@@ -28,8 +28,27 @@ app.get('/todos', (req, res) => {
     Todo.find().then(todos => res.send({todos}), err => res.status(400).send(err));
 });
 
-app.post('/user', (req, res) => {
+app.get('/todos/:id', (req, res) => {
 
+    let id = req.params.id
+
+    // validate id
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then(todo => {
+        // not found
+        if (!todo) {
+            return res.status(404).send();
+        }
+        // found
+        res.send({todo});
+    }).catch(err => res.status(400).send());
+});
+
+app.post('/user', (req, res) => {
+``
     let user = new User({
         email: req.body.email
     });
